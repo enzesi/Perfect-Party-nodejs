f2f87b5afe11976ca44d3b147af9ceae7bf69c38
enzesi <32287882+enzesi@users.noreply.github.com>
2019-07-16 20:17:16 -0400

    connected to postgres

---
+++ services/client.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    getClient: async function(params) {
+         
+    }
+    
+}
---
+++ services/services.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    getParty: async function(params) {
+         console.log(1)
+    },
+    readParty: async function(){
+
+    },
+    
+}
